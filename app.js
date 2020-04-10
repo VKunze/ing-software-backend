0ca1f0a246988c43bc53be7d02d5ae6bdea2477b
Seba <sebastiancura97@gmail.com>
2020-04-10 16:53:47 -0300

    remove server.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(solicitudesRouter);
 app.use(productosRouter);
 
 // // set port, listen for requests
-// const PORT = process.env.PORT || 8080;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}.`);
-// });
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
