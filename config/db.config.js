408ba43aced4ee114a99eb0a5be2840e2390a3e5
Seba <sebastiancura97@gmail.com>
2020-04-10 12:49:49 -0300

    Prepare project to Heroku

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "ingsoftwareii",
-    dialect: "mysql",
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB,
+    dialect: process.env.dialect,
   };
