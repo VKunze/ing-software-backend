0b5e483c5e8d4e04d9c27f9381e9c254892f85bd
Jose Pedro Chifflet <josepedro@decemberlabs.com>
2020-04-18 16:42:13 -0300

    endpoint to pictures

---
+++ controllers/assets.controller.js
@@ -0,0 +1,22 @@
+exports.handleAssets = async (req, res) => {
+  try {
+    const {idCardPicture , cameraPicture} = req.body;
+    if (!idCardPicture || !cameraPicture) {
+      res.status(400).send({
+        success: false,
+        code: 'BAD_REQUEST',
+        message: 'Assets not received.'
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+      });
+    }
+  } catch (e) {
+    res.status(500).send({
+      success: false,
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'Ha ocurrido un error inesperado, intente de nuevo mas tarde!'
+    });
+  }
+}
