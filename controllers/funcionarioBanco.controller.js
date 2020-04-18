030bb2f2f44a8e5fbdf62f5468ca2252d4d1aa26
Jose Pedro Chifflet <josepedro@decemberlabs.com>
2020-04-18 18:41:23 -0300

    login fix

--- controllers/funcionarioBanco.controller.js
+++ controllers/funcionarioBanco.controller.js
@@ -1,5 +1,6 @@
 var funcionarioBancoService = require("../services/funcionarioBanco.service.js");
-var helpers = require('../utils/helpers.js');
+var helpers = require("../utils/helpers.js");
+const bcrypt = require("bcrypt");
 
 exports.checkdb = async (req, res) => {
   try {
@@ -8,39 +9,41 @@ exports.checkdb = async (req, res) => {
     if (!usuario || !contraseña) {
       res.status(400).send({
         success: false,
-        code: 'BAD_REQUEST',
-        message: 'Ingrese usuario/contraseña'
+        code: "BAD_REQUEST",
+        message: "Ingrese usuario/contraseña",
       });
     }
-    const userFromDb = await funcionarioBancoService.findOne(usuario, contraseña);
-    if (!userFromDb) {
+    const userFromDb = await funcionarioBancoService.findOne(usuario);
+    if (!userFromDb || !bcrypt.compareSync(contraseña, userFromDb["contraseña"])) {
       res.status(400).send({
         success: false,
-        code: 'INVALID_CREDENTIALS',
-        message: 'Usuario y/o contraseña incorrectos'
+        code: "INVALID_CREDENTIALS",
+        message: "Usuario y/o contraseña incorrectos",
       });
     } else {
       var token = helpers.generateJWTToken(
         {
           usuario: usuario,
-          contraseña: contraseña
+          contraseña: contraseña,
         },
-        'secretproyectkey', '60m', usuario
+        "secretproyectkey",
+        "60m",
+        usuario
       );
       res.status(200).send({
         success: true,
         username: userFromDb.usuario,
-        token: token
+        token: token,
       });
     }
   } catch (e) {
     res.status(500).send({
       success: false,
-      code: 'INTERNAL_SERVER_ERROR',
-      message: 'Ha ocurrido un error inesperado, intente de nuevo mas tarde!'
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
     });
   }
-}
+};
 
 exports.create = async (req, res) => {
   try {
@@ -49,19 +52,19 @@ exports.create = async (req, res) => {
     if (!usuario || !contraseña) {
       res.status(400).send({
         success: false,
-        code: 'BAD_REQUEST',
-        message: 'Ingrese usuario/contraseña'
+        code: "BAD_REQUEST",
+        message: "Ingrese usuario/contraseña",
       });
     }
     const respuesta = await funcionarioBancoService.create(usuario, contraseña);
     res.status(200).send({
-      success: true
+      success: true,
     });
   } catch (e) {
     res.status(500).send({
       success: false,
-      code: 'INTERNAL_SERVER_ERROR',
-      message: 'Ha ocurrido un error inesperado, intente de nuevo mas tarde!'
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
     });
   }
-}
+};
--- services/funcionarioBanco.service.js
+++ services/funcionarioBanco.service.js
@@ -1,49 +1,42 @@
 const db = require("../db/index.js");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const FuncionarioBanco = db.funcionarioBanco;
 const Op = db.Sequelize.Op;
 
 // Find a single usuario and contraseña
-exports.findOne = async (usuario, contraseña) => {
-    // const contraseñaHasheada = await hashContraseña(contraseña);
-    return FuncionarioBanco.findOne({ where: { usuario: usuario } })
-        .then(data => {
-            return bcrypt.compare(contraseña, data.contraseña, function (err, res) {
-                if (res) {
-                    return data;
-                } else {
-                    return false;
-                }
-            });
-        })
-        .catch(err => {
-            throw err;
-        });
+exports.findOne = async (usuario) => {
+  // const contraseñaHasheada = await hashContraseña(contraseña);
+  return FuncionarioBanco.findOne({ where: { usuario } })
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
 exports.create = async (usuario, contraseña) => {
-    bcrypt.hash(contraseña, 10, function (err, hash) {
-        if (err) {
-            throw err;
-        }
-        FuncionarioBanco.create({ usuario: usuario, contraseña: hash })
-            .then(data => {
-                return (data);
-            })
-            .catch(e => {
-                throw e;
-            });
-    });
-}
+  bcrypt.hash(contraseña, 10, function (err, hash) {
+    if (err) {
+      throw err;
+    }
+    FuncionarioBanco.create({ usuario: usuario, contraseña: hash })
+      .then((data) => {
+        return data;
+      })
+      .catch((e) => {
+        throw e;
+      });
+  });
+};
 
 async function hashContraseña(contraseña) {
-    const contraseñaHasheada = await new Promise((resolve, reject) => {
-        bcrypt.hash(contraseña, 10, function (err, hash) {
-            if (err) reject(err)
-            resolve(hash)
-        });
+  const contraseñaHasheada = await new Promise((resolve, reject) => {
+    bcrypt.hash(contraseña, 10, function (err, hash) {
+      if (err) reject(err);
+      resolve(hash);
     });
-    console.log("in func: " + contraseñaHasheada);
-    return contraseñaHasheada
+  });
+  console.log("in func: " + contraseñaHasheada);
+  return contraseñaHasheada;
 }
-
