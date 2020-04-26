cb02c35efb37e884aae74825974b219231044141
Juan José Regent <jregent@correo.um.edu.uy>
2020-04-26 19:18:18 -0300

    middleware en ingles

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 var jwt = require('jsonwebtoken'); //used to create, sign and verify tokens
-var funcionarioBancoService = require("./services/funcionarioBanco.service.js");
+var bankWorkerService = require("./services/bankWorker.service.js");
 require("dotenv").config();
 
 async function authenticateToken(req, res, next){
@@ -7,7 +7,7 @@ async function authenticateToken(req, res, next){
     const token = authHeader && authHeader.split(' ')[1]            
     
     // buscar usuario en la db por token
-    const candidate =  await funcionarioBancoService.findByToken(token);
+    const candidate =  await bankWorkerService.findByToken(token);
 
     if(token == null) return res.sendStatus(401)
     jwt.verify(token, process.env.SECRET_KEY, (err, content) => {
