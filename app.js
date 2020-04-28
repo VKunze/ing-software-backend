e2ce1487263c8a36d19aefe44c3454f512dc10c0
Seba <sebastiancura97@gmail.com>
2020-04-28 10:02:59 -0300

    remove onapplication

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 
-app.use(express.static(__dirname + "/onApplication"));
+app.use(express.static(__dirname ));
 
 
 db.sequelize.sync({force:false});
