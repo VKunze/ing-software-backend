a7bf606b423e747b9941303a4d569872e6451983
Jose Pedro Chifflet <josepedro@decemberlabs.com>
2020-04-28 10:28:58 -0300

    fix config vars

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,16 +1,16 @@
 const config = require("../config/config");
 
-var cloudinary = require('cloudinary');
+var cloudinary = require("cloudinary");
 
 cloudinary.config({
-  cloud_name: config.CLOUDINARY_CLOUD_NAME,
-  api_key: config.CLOUDINARY_API_KEY,
-  api_secret: config.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 exports.uploadImage = (id, image) => {
   return new Promise((resolve, reject) => {
-    cloudinary.v2.uploader.upload(`data:image/jpg;base64,${image}`, {public_id: id}, function(
+    cloudinary.v2.uploader.upload(`data:image/jpg;base64,${image}`, { public_id: id }, function (
       error,
       result
     ) {
