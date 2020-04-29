f84e2db8ef6916d7a6cad60af14737966fc2c558
Jose Pedro Chifflet <josepedro@decemberlabs.com>
2020-04-29 17:13:47 -0300

    fix

--- db/models/index.js
+++ db/models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
-  port: dbConfig.PORT
+  port: dbConfig.PORT,
 });
 
 const db = {};
@@ -16,6 +16,7 @@ db.bankWorker = require("./bankWorker.db.js")(sequelize, Sequelize);
 db.state = require("./states.db.js")(sequelize, Sequelize);
 db.product = require("./product.db.js")(sequelize, Sequelize);
 db.solicitude = require("./solicitude.db.js")(sequelize, Sequelize);
+db.debug = require("./debug.db.js")(sequelize, Sequelize);
 
 // Relationships between tables
 db.product.hasMany(db.solicitude);
