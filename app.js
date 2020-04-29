d470c49f785c8a83a2af1272c2676a0b0890f748
Jose Pedro Chifflet <josepedro@decemberlabs.com>
2020-04-29 16:03:36 -0300

    cors for prefligthed reqs

--- app.js
+++ app.js
@@ -4,18 +4,48 @@ const cors = require("cors");
 const db = require("./db/models/index.js");
 const apiRoutes = require("./api/index");
 
+const corsOptionsHandler = (req, callback) => {
+  const corsOptions = {
+    origin(origin, call) {
+      if (!origin) {
+        call(null, true);
+      } else {
+        Sentry.captureMessage(JSON.stringify(req));
+        call(new Forbidden("Not allowed"), false);
+      }
+    },
+    credentials: true,
+    allowedHeaders: [
+      "Save-Data",
+      "Content-Type",
+      "Authorization",
+      "Content-Length",
+      "X-Requested-With",
+      "Accept",
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  };
+  callback(null, corsOptions);
+};
+
 var app = express();
 require("dotenv").config();
 
-app.use(cors());
-app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
+app.use((req, res, next) => {
+  res.header("Vary", "Origin");
+  res.header("Cache-Control", "no-cache,must-revalidate");
+  next();
+});
+app.use(cors(corsOptionsHandler));
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 
-app.use(express.static(__dirname ));
+app.use(express.static(__dirname));
 
-
-db.sequelize.sync({force:false});
-app.use('', apiRoutes);
+db.sequelize.sync({ force: false });
+app.use("", apiRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
