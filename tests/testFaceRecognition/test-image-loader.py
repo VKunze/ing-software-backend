2a204f56abe16089e1b3419f519aa21ea71947c7
VKunze <39528476+VKunze@users.noreply.github.com>
2020-05-13 15:59:38 -0300

    cuentas de metricas face-recog test

--- tests/testFaceRecognition/test-image-loader.py
+++ tests/testFaceRecognition/test-image-loader.py
@@ -1,15 +1,22 @@
 from api-test import *
 
-def calculateErrorRate():
-    
+def getMainMetrics(resultadosImgValidas, resultadosImgNoValidas):
+    TP = resultadosImgValidas[0]
+    FN = resultadosImgValidas[1]
+    TN = resultadosImgNoValidas[0]
+    FP = resultadosImgNoValidas[1]
+    return FP, FN, TP, TN
 
-def calculateFMR():
+def calculateErrorRate(FP, FN, TP, TN):
+    return (FP + FN) / (TP + TN + FP + FN) 
 
+def calculateFMR(FP, TP):
+    return FP / (FP + TP)
 
 def loadImages():
     #Cargar imagenes de carar válidas
     resultadosImgValidas = []
-
+    
 
     #Cargar imagenes de la caras no validas
     resultadosImgNoValidas = []
@@ -19,12 +26,12 @@ def loadImages():
     [FP, FN, TP, TN] = getMainMetrics(resultadosImgValidas, resultadosImgNoValidas)
 
     #Calcular Error rate
-    errorRate = calculateErrorRate()
+    errorRate = calculateErrorRate(FP, FN, TP, TN)
 
     #Calcular False match rate (FMR)
     #Probabilidad de que el sistema acepte incorrectamente. Es el porcentaje de caras que no coinciden 
     #que son aceptadas por el sistema. 
-    FRM = calculateFMR()
+    FRM = calculateFMR(FP, TP)
 
     return [errorRate, FRM]
 
