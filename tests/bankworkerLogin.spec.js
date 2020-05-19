bb013842a1a7b2c10273da0f634d7e55dad6195a
Juan José Regent <jregent@correo.um.edu.uy>
2020-05-19 19:23:13 -0300

    tokens test

--- tests/bankworkerLogin.spec.js
+++ tests/bankworkerLogin.spec.js
@@ -1,6 +1,7 @@
 var assert = require("assert");
 const bcrypt = require("bcrypt");
 var bankWorkerHelper = require("../api/bankWorker/bankWorker.service");
+var utilsJWTHelper = require("../utils/helpers");
 
 const URL_REGEX = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
 
@@ -51,5 +52,42 @@ describe("BankWorker", function () {
 
       assert.equal(false, result);
     });
+
+    it("generate token and save token on db", async function () {
+      const user = "admin12345";
+      const password = "admin12345";
+
+      const token = utilsJWTHelper.generateJWTToken({
+        username: user,
+        password: password
+      });
+
+      if (!token) {
+        assert.fail();
+      }
+      assert.ok(true);
+
+      const userFromDB = await bankWorkerHelper.findOne(user);
+
+      if (!userFromDB) {
+        assert.fail();
+      }
+
+      await bankWorkerHelper.saveToken(token, userFromDB.id);
+
+      const userFromDBupdated = await bankWorkerHelper.findOne(user);
+
+      if (!userFromDBupdated) {
+        assert.fail();
+      }
+      var result = false;
+      if (token == userFromDBupdated.token) {
+        result = true;
+      }
+
+      assert.equal(true, result);
+    });
+
   });
+
 });
