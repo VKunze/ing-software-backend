d2185d8edb58f44d2c65c03f7fcb57767236c500
Stephanie <smendoncasp@gmail.com>
2020-06-08 17:57:42 -0300

    arreglos terminados

--- api/applications/applications.controller.js
+++ api/applications/applications.controller.js
@@ -3,141 +3,146 @@ const db = require("../../db/models/index.js");
 const Debug = db.debug;
 const notificationHelper = require("./../../utils/notifications.js");
 
-exports.generateApplication = async (req, res) => {
-  try {
-    const solicitudeJson = req.body;
-    if (!solicitudeJson) {
-      return res.status(400).send({
-        success: false,
-        code: "BAD_REQUEST",
-        message: "Ingrese una solicitude",
-      });
+exports.generateApplication = async(req, res) => {
+    try {
+        const solicitudeJson = req.body;
+        if (!solicitudeJson) {
+            return res.status(400).send({
+                success: false,
+                code: "BAD_REQUEST",
+                message: "Ingrese una solicitude",
+            });
+        }
+        const respuesta = await applicationsService.save(solicitudeJson);
+        console.log("respuesta:", respuesta);
+        if (typeof respuesta == "string" && respuesta.includes("Invalid")) {
+            var message = respuesta.includes("product") ? "Product" : "Data";
+            return res.status(400).send({
+                success: false,
+                code: "BAD_REQUEST",
+                message: "Invalid " + message,
+            });
+        } else {
+            notificationHelper.sendPushNotificationToAdmins().catch((e) => {
+                console.log(e);
+            });
+            return res.status(200).send({
+                success: true,
+                estado: respuesta,
+            });
+        }
+    } catch (e) {
+        return res.status(500).send({
+            success: false,
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
+        });
     }
-    const respuesta = await applicationsService.save(solicitudeJson);
-    console.log("respuesta:", respuesta);
-    if (typeof respuesta == "string" && respuesta.includes("Invalid")) {
-      var message = respuesta.includes("product") ? "Product" : "Data";
-      return res.status(400).send({
-        success: false,
-        code: "BAD_REQUEST",
-        message: "Invalid " + message,
-      });
-    } else {
-      notificationHelper.sendPushNotificationToAdmins().catch((e) => {
+};
+
+exports.compareFotos = async(req, res) => {
+    try {
+        const userIdCardNumber = req.body.userIdCardNumber;
+        const fotoCedula = req.body.fotoCedula;
+        const fotoSelfie = req.body.fotoSelfie;
+        if (!fotoCedula || !fotoSelfie) {
+            res.status(400).send({
+                success: false,
+                code: "BAD_REQUEST",
+                message: "Ingrese una foto de la cedula y una selfie",
+            });
+        }
+        const resultOfComparison = await applicationsService.compareFotos(
+            userIdCardNumber,
+            fotoCedula,
+            fotoSelfie
+        );
+        if (
+            resultOfComparison.toUpperCase().includes("ERROR") &&
+            resultOfComparison.includes("NoFace")
+        ) {
+            res.status(400).send({
+                success: false,
+                result: false,
+                code: "NO_FACE",
+                message: "No ha sido posible detectar un rostro en la imagen enviada.",
+            });
+        } else if (resultOfComparison.toUpperCase().includes("ERROR")) {
+            res.status(400).send({
+                success: false,
+                result: false,
+                code: "VALIDATION_FAILED",
+                message: "No ha sido posible detectar el error que deneg",
+            });
+        } else {
+            res.status(200).send({
+                success: true,
+                result: resultOfComparison.includes("True") ? true : false,
+            });
+        }
+    } catch (e) {
         console.log(e);
-      });
-      return res.status(200).send({
-        success: true,
-        estado: respuesta,
-      });
+        res.status(500).send({
+            success: false,
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
+            trace: e,
+        });
     }
-  } catch (e) {
-    return res.status(500).send({
-      success: false,
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
-    });
-  }
 };
 
-exports.compareFotos = async (req, res) => {
-  try {
-    const userIdCardNumber = req.body.userIdCardNumber;
-    const fotoCedula = req.body.fotoCedula;
-    const fotoSelfie = req.body.fotoSelfie;
-    if (!fotoCedula || !fotoSelfie) {
-      res.status(400).send({
-        success: false,
-        code: "BAD_REQUEST",
-        message: "Ingrese una foto de la cedula y una selfie",
-      });
+exports.getAllPendingApplications = async(req, res) => {
+    try {
+        const apps = await applicationsService.getAllPendingApplications();
+        res.status(200).send({
+            success: true,
+            applications: apps,
+            message: "Apps",
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({
+            success: false,
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
+        });
     }
-    const resultOfComparison = await applicationsService.compareFotos(
-      userIdCardNumber,
-      fotoCedula,
-      fotoSelfie
-    );
-    if (
-      resultOfComparison.toUpperCase().includes("ERROR") &&
-      resultOfComparison.includes("NoFace")
-    ) {
-      res.status(400).send({
-        success: false,
-        result: false,
-        code: "NO_FACE",
-        message: "No ha sido posible detectar un rostro en la imagen enviada.",
-      });
-    } else if (resultOfComparison.toUpperCase().includes("ERROR")) {
-      res.status(400).send({
-        success: false,
-        result: false,
-        code: "VALIDATION_FAILED",
-        message: "No ha sido posible detectar el error que deneg",
-      });
-    } else {
-      res.status(200).send({
-        success: true,
-        result: resultOfComparison.includes("True") ? true : false,
-      });
-    }
-  } catch (e) {
-    console.log(e);
-    res.status(500).send({
-      success: false,
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
-      trace: e,
-    });
-  }
 };
 
-exports.getAllPendingApplications = async (req, res) => {
-  try {
-    const apps = await applicationsService.getAllPendingApplications();
-    res.status(200).send({
-      success: true,
-      applications: apps,
-      message: "Apps",
-    });
-  } catch (e) {
-    console.log(e);
-    res.status(500).send({
-      success: false,
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
-    });
-  }
-};
+exports.updateState = async(req, res) => {
+    try {
+        const idSolicitude = req.body.idSolicitude;
+        const newState = req.body.state;
+        if (!idSolicitude || !newState) {
+            res.status(400).send({
+                success: false,
+                code: "BAD_REQUEST",
+                message: "Ingrese idSolicitude/state",
+            });
+        }
+        const respuesta = await applicationsService.updateState(idSolicitude, newState);
+        var ci = await applicationsService.getCedula(idSolicitude);
+        notificationHelper.sendPushNotificationToAppliants([ci]).catch((e) => {
+            console.log(e);
+        });
 
-exports.updateState = async (req, res) => {
-  try {
-    const idSolicitude = req.body.idSolicitude;
-    const newState = req.body.state;
-    if (!idSolicitude || !newState) {
-      res.status(400).send({
-        success: false,
-        code: "BAD_REQUEST",
-        message: "Ingrese idSolicitude/state",
-      });
+        if (typeof respuesta == "string" && respuesta.includes("Invalid")) {
+            res.status(400).send({
+                success: false,
+                code: "BAD_REQUEST",
+                message: respuesta,
+            });
+        } else {
+            res.status(200).send({
+                success: true,
+            });
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).send({
+            success: false,
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
+        });
     }
-    const respuesta = await applicationsService.updateState(idSolicitude, newState);
-    if (typeof respuesta == "string" && respuesta.includes("Invalid")) {
-      res.status(400).send({
-        success: false,
-        code: "BAD_REQUEST",
-        message: respuesta,
-      });
-    } else {
-      res.status(200).send({
-        success: true,
-      });
-    }
-  } catch (e) {
-    console.log(e);
-    res.status(500).send({
-      success: false,
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Ha ocurrido un error inesperado, intente de nuevo mas tarde!",
-    });
-  }
 };
--- api/applications/applications.service.js
+++ api/applications/applications.service.js
@@ -9,158 +9,168 @@ const Solicitude = db.solicitude;
 const Product = db.product;
 const State = db.state;
 
-exports.save = async (datosSolicitude) => {
-  printSystem.startClock();
-  var productId = "";
-  var stateId = "";
-  try {
-    productId = await getProductId(datosSolicitude.producto);
-  } catch (err) {
-    return "Invalid product or state";
-  }
-  try {
-    const solicitudeBdd = {
-      productId: productId,
-      personFirstName: datosSolicitude.nombre,
-      personLastName: datosSolicitude.apellido,
-      personCedula: datosSolicitude.cedula,
-      personAddress: datosSolicitude.direccion,
-      personSalary: datosSolicitude.sueldo,
-      personDeliveryAddress: datosSolicitude.direccionEntrega,
-    };
-
-    for (key in solicitudeBdd) {
-      if (solicitudeBdd[key] == undefined) {
-        return "Invalid data";
-      }
+exports.save = async(datosSolicitude) => {
+    printSystem.startClock();
+    var productId = "";
+    var stateId = "";
+    try {
+        productId = await getProductId(datosSolicitude.producto);
+    } catch (err) {
+        return "Invalid product or state";
     }
-    console.log(solicitudeBdd);
-    //Create a solicitude in db
-    var createdInstance = await Solicitude.create(solicitudeBdd).then((data) => {
-      return data;
-    });
-    var stateId = processRiskPorcentage.processRiskPorcentage(solicitudeBdd.productId, solicitudeBdd.personCedula, solicitudeBdd.personSalary);
-    this.updateState(createdInstance.id, stateId);
-    return stateId;
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
+    try {
+        const solicitudeBdd = {
+            productId: productId,
+            personFirstName: datosSolicitude.nombre,
+            personLastName: datosSolicitude.apellido,
+            personCedula: datosSolicitude.cedula,
+            personAddress: datosSolicitude.direccion,
+            personSalary: datosSolicitude.sueldo,
+            personDeliveryAddress: datosSolicitude.direccionEntrega,
+        };
+
+        for (key in solicitudeBdd) {
+            if (solicitudeBdd[key] == undefined) {
+                return "Invalid data";
+            }
+        }
+        console.log(solicitudeBdd);
+        //Create a solicitude in db
+        var createdInstance = await Solicitude.create(solicitudeBdd).then((data) => {
+            return data;
+        });
+        var stateId = processRiskPorcentage.processRiskPorcentage(solicitudeBdd.productId, solicitudeBdd.personCedula, solicitudeBdd.personSalary);
+        this.updateState(createdInstance.id, stateId);
+        return stateId;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
 };
 
-exports.compareFotos = async (userId, base64Ci, base64User) => {
-  try {
-    const urlCameraPhoto = await cloudinaryHelper.uploadImage(`${userId}_camera`, base64User);
-    const urlCiPhoto = await cloudinaryHelper.uploadImage(`${userId}_ci_card`, base64Ci);
+exports.compareFotos = async(userId, base64Ci, base64User) => {
+    try {
+        const urlCameraPhoto = await cloudinaryHelper.uploadImage(`${userId}_camera`, base64User);
+        const urlCiPhoto = await cloudinaryHelper.uploadImage(`${userId}_ci_card`, base64Ci);
 
-    const pythonScriptPromise = async () => {
-      var dataPythonResult;
-      // must be adapted for linux paths \\ braces are for windows
-      // const python = spawn("python", [
-      //   "./utils/comparator.py",
-      //   `\\onApplication\\${userId}_ci_card_picture.jpg`,
-      //   `\\onApplication\\${userId}_camera_picture.jpg`,
-      // ]);
-      const python = spawn("python", ["./utils/comparator.py", urlCiPhoto.url, urlCameraPhoto.url]);
-      return new Promise((resolve, reject) => {
-        var error;
-        python.stdout.on("data", function (data) {
-          dataPythonResult = data.toString();
-        });
+        const pythonScriptPromise = async() => {
+            var dataPythonResult;
+            // must be adapted for linux paths \\ braces are for windows
+            // const python = spawn("python", [
+            //   "./utils/comparator.py",
+            //   `\\onApplication\\${userId}_ci_card_picture.jpg`,
+            //   `\\onApplication\\${userId}_camera_picture.jpg`,
+            // ]);
+            const python = spawn("python", ["./utils/comparator.py", urlCiPhoto.url, urlCameraPhoto.url]);
+            return new Promise((resolve, reject) => {
+                var error;
+                python.stdout.on("data", function(data) {
+                    dataPythonResult = data.toString();
+                });
 
-        python.stderr.on("data", function (data) {
-          error = data;
-        });
+                python.stderr.on("data", function(data) {
+                    error = data;
+                });
 
-        python.on("close", (code) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(dataPythonResult);
-          }
+                python.on("close", (code) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(dataPythonResult);
+                    }
+                });
+            });
+        };
+        const result = await pythonScriptPromise().catch(err => {
+            return "ERROR:" + err.toString();
         });
-      });
-    };
-    const result = await pythonScriptPromise().catch(err => {
-      return "ERROR:" + err.toString();
-    });
-    return result;
-  } catch (err) {
-    throw new Error(err);
-  }
+        return result;
+    } catch (err) {
+        throw new Error(err);
+    }
 };
 
 exports.getAllPendingApplications = () => {
-  return Solicitude.findAll({
-    include: [{
-      model: State,
-      where: {
-        name: "Esperando aprobacion"
-      }
-    }]
-  }).then((data) => {
-    // console.log(data);
-    return data;
-  });
+    return Solicitude.findAll({
+        include: [{
+            model: State,
+            where: {
+                name: "Esperando aprobacion"
+            }
+        }]
+    }).then((data) => {
+        // console.log(data);
+        return data;
+    });
 }
 
-exports.updateState = async (idSolicitude, newState) => {
-  try {
-    var solicitude = await Solicitude.findByPk(idSolicitude);
-    //console.log(solicitude);
-    //console.log("new state: ", newState);
-    var state = await State.findOne({ where: { name: newState } });
-    //console.log(state);
-    if (solicitude === null || state === null) {
-      return "Invalid solicitude ID/ state";
+exports.updateState = async(idSolicitude, newState) => {
+    try {
+        var solicitude = await Solicitude.findByPk(idSolicitude);
+        //console.log(solicitude);
+        //console.log("new state: ", newState);
+        var state = await State.findOne({ where: { name: newState } });
+        //console.log(state);
+        if (solicitude === null || state === null) {
+            return "Invalid solicitude ID/ state";
+        }
+        solicitude.setState(state);
+        return solicitude.save();
+    } catch (err) {
+        throw err;
     }
-    solicitude.setState(state);
-    return solicitude.save();
-  } catch (err) {
-    throw err;
-  }
 }
 
 function getProductId(nombreProducto) {
-  //console.log(nombreProducto);
-  return Product.findOne({ where: { name: nombreProducto } })
-    .then((data) => {
-      return data.id;
-    })
-    .catch((err) => {
-      throw (err);
-    });
+    //console.log(nombreProducto);
+    return Product.findOne({ where: { name: nombreProducto } })
+        .then((data) => {
+            return data.id;
+        })
+        .catch((err) => {
+            throw (err);
+        });
 }
 
 function getIdInicialState() {
-  return State.findOne({ where: { name: "Esperando aprobacion" } })
-    .then((data) => {
-      return data.id;
-    })
-    .catch((err) => {
-      throw (err);
-    });
+    return State.findOne({ where: { name: "Esperando aprobacion" } })
+        .then((data) => {
+            return data.id;
+        })
+        .catch((err) => {
+            throw (err);
+        });
 }
 
 function getIdState(nombre) {
-  return State.findOne({ where: { name: nombre } })
-    .then((data) => {
-      return data.id;
-    })
-    .catch((err) => {
-      console.log(err);
-      throw (err);
-    });
+    return State.findOne({ where: { name: nombre } })
+        .then((data) => {
+            return data.id;
+        })
+        .catch((err) => {
+            console.log(err);
+            throw (err);
+        });
 }
 
 function getNameState(id) {
-  return State.findOne({ where: { id: id } })
-    .then((data) => {
-      return data.nombre;
-    })
-    .catch((err) => {
-      throw (err);
-    });
+    return State.findOne({ where: { id: id } })
+        .then((data) => {
+            return data.nombre;
+        })
+        .catch((err) => {
+            throw (err);
+        });
 }
 
-exports = getNameState, getIdState;
+function getCedula(id) {
+    return Solicitude.findOne({ where: { id: id } })
+        .then((data) => {
+            return data.personCedula;
+        })
+        .catch((err) => {
+            throw (err);
+        });
+}
+
+exports = getNameState, getIdState, getCedula;
