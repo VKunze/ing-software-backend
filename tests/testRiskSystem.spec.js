04a9eec66a0a4970ed1e8b82f8b9cf91b0be5aca
VKunze <39528476+VKunze@users.noreply.github.com>
2020-06-17 10:24:38 -0300

    comment test printsystem

--- tests/testRiskSystem.spec.js
+++ tests/testRiskSystem.spec.js
@@ -1,8 +1,8 @@
- var assert = require("assert");
- const riskSystem = require("../externalSystems/riskSystem/processRiskPorcentage.js");
+var assert = require("assert");
+const riskSystem = require("../externalSystems/riskSystem/processRiskPorcentage.js");
 
- describe("testPrintSystem", function() {
-     it('should return "Aprobada', function() {
-         assert.equal('Aprobada', riskSystem.processRiskPorcentage("Gold", "355874644", "30000"));
-     })
- });
+describe("testRiskSystem", function () {
+    it('should return Rechazada', function () {
+        assert.equal('Rechazada', riskSystem.processRiskPorcentage("Gold", "355874644", "30000"));
+    })
+}); 
