568edefd95e6e5c58aa643368f04e99261ff5621
Seba <sebastiancura97@gmail.com>
2020-06-22 17:37:53 -0300

    fix test

--- tests/testRiskSystem.spec.js
+++ tests/testRiskSystem.spec.js
@@ -2,7 +2,10 @@ var assert = require("assert");
 const riskSystem = require("../externalSystems/riskSystem/processRiskPorcentage.js");
 
 describe("testRiskSystem", function () {
-    it('should return Aprobada', function () {
-        assert.equal('Aprobada', riskSystem.processRiskPorcentage("Gold", "355874644", "30000"));
-    })
+    it("should return Esperando aprobacion", function () {
+      assert.equal(
+        "Esperando aprobacion",
+        riskSystem.processRiskPorcentage("Gold", "355874644", "30000")
+      );
+    });
 }); 
